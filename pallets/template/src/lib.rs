006d30754a69b76e5bd71873b01876d2d1a9d46a
r1oga <r1oga@tuta.io>
2020-11-08 22:24:21 +0100

    feat: Add struct to storage in template pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,8 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode};
 use frame_support::{decl_error, decl_event, decl_module, decl_storage, ensure, StorageMap};
 use frame_system::ensure_signed;
+use sp_runtime::traits::BlakeTwo256;
+use sp_runtime::traits::Hash;
 use sp_std::vec::Vec;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct Test {
+	positive: bool,
+	tester: Vec<u8>,
+}
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -14,11 +23,13 @@ pub trait Trait: frame_system::Trait {
 // Event documentation should end with an array that provides descriptive names for parameters.
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event! {
-	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-			/// Event emitted when a proof has been claimed. [who, claim]
-			ClaimCreated(AccountId, Vec<u8>),
-			/// Event emitted when a claim is revoked by the owner. [who, claim]
-			ClaimRevoked(AccountId, Vec<u8>),
+	pub enum Event<T> where Hash = <T as frame_system::Trait>::Hash {
+			/// Event emitted when a test is published for the first time:
+			/// [tester, tested, postive True/False]
+			TestPublished(Vec<u8>, Hash, bool),
+			/// Event emitted when an existing test is updated:
+			/// [tester, tested, postive True/False]
+			TestUpdated(Vec<u8>, Hash, bool),
 	}
 }
 
@@ -26,11 +37,11 @@ decl_event! {
 decl_error! {
 	pub enum Error for Module<T: Trait> {
 			/// The proof has already been claimed.
-			ProofAlreadyClaimed,
-			/// The proof does not exist, so it cannot be revoked.
-			NoSuchProof,
-			/// The proof is claimed by another account, so caller can't revoke it.
-			NotProofOwner,
+			TestAlreadyPublished,
+			/// Tester can't be None value
+			TesterIsNone,
+			/// Test does not exist so it can't be updated
+			NoSuchTest
 	}
 }
 
@@ -40,7 +51,7 @@ decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
 			/// The storage item for our proofs.
 			/// It maps a proof to the user who made the claim and when they made it.
-			Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+			Tests get(fn test): map hasher(blake2_128_concat) T::Hash => Test;
 	}
 }
 
@@ -55,49 +66,55 @@ decl_module! {
 			// Events must be initialized if they are used by the pallet.
 			fn deposit_event() = default;
 
-			/// Allow a user to claim ownership of an unclaimed proof.
+			/// Allow a user to publish a test and store it in storage.
 			#[weight = 10_000]
-			fn create_claim(origin, proof: Vec<u8>) {
+			fn insert_test(origin, tested: Vec<u8>, tester: Vec<u8>, positive:bool) {
 					// Check that the extrinsic was signed and get the signer.
 					// This function will return an error if the extrinsic is not signed.
 					// https://substrate.dev/docs/en/knowledgebase/runtime/origin
 					let sender = ensure_signed(origin)?;
 
-					// Verify that the specified proof has not already been claimed.
-					ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+					// hash name of tested patient
+					let nonce = 0;
+					let seed = BlakeTwo256::hash(&tested);
+					let hash = (seed, &sender, nonce).using_encoded(<T as frame_system::Trait>::Hashing::hash);
 
-					// Get the block number from the FRAME System module.
-					let current_block = <frame_system::Module<T>>::block_number();
+					// Verify that the test isn't yet in the map.
+					ensure!(!Tests::<T>::contains_key(&hash), Error::<T>::TestAlreadyPublished);
+					// Insert Test in Storage map
+					let test = Test {
+						positive: positive,
+						tester: tester.clone(),
+					};
 
-					// Store the proof with the sender and block number.
-					Proofs::<T>::insert(&proof, (&sender, current_block));
+					<Tests<T>>::insert(&hash, &test);
 
-					// Emit an event that the claim was created.
-					Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+					// Emit event
+					Self::deposit_event(RawEvent::TestPublished(tester, hash, positive));
 			}
-
-			/// Allow the owner to revoke their claim.
+			/// Modify a test result
 			#[weight = 10_000]
-			fn revoke_claim(origin, proof: Vec<u8>) {
+			fn update_test(origin, tested: Vec<u8>, tester: Vec<u8>, positive:bool) {
 					// Check that the extrinsic was signed and get the signer.
 					// This function will return an error if the extrinsic is not signed.
 					// https://substrate.dev/docs/en/knowledgebase/runtime/origin
 					let sender = ensure_signed(origin)?;
 
-					// Verify that the specified proof has been claimed.
-					ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+					// hash name of tested patient
+					let nonce = 0;
+					let seed = BlakeTwo256::hash(&tested);
+					let hash = (seed, &sender, nonce).using_encoded(<T as frame_system::Trait>::Hashing::hash);
+					// Verify that the test has already been published
+					ensure!(Tests::<T>::contains_key(&hash), Error::<T>::NoSuchTest);
 
-					// Get owner of the claim.
-					let (owner, _) = Proofs::<T>::get(&proof);
+					// Mutate test in storage.
+					Tests::<T>::mutate_exists(hash, |_| Test {
+						positive: positive,
+						tester: tester.clone(),
+					});
 
-					// Verify that sender of the current call is the claim owner.
-					ensure!(sender == owner, Error::<T>::NotProofOwner);
-
-					// Remove claim from storage.
-					Proofs::<T>::remove(&proof);
-
-					// Emit an event that the claim was erased.
-					Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
+					// Emit an event that the test was updated.
+					Self::deposit_event(RawEvent::TestUpdated(tester, hash, positive));
 			}
 	}
 }
